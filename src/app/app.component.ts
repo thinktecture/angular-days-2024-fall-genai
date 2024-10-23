2866e4ba94837bf8a703d57f89f7edc4743e1e85
Christian Liebel <christian.liebel@thinktecture.com>
2024-10-23 11:52:54 +0200

    Lab #7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,11 @@ export class AppComponent implements OnInit {
     // LAB #3, #7, #8 and #9
     await this.engine!.resetChat();
     this.reply.set('…');
+    // const systemPrompt = `Here's the user's todo list: \`\`\`json${JSON.stringify(this.todos())}\`\`\``;
+    const systemPrompt = `Here's the user's todo list:
+      ${this.todos().map(todo => `* ${todo.text} (${todo.done ? 'done' : 'not done'})`).join('\n')}`;
     const messages: ChatCompletionMessageParam[] = [
+      { role: "system", content: systemPrompt },
       { role: "user", content: userPrompt }
     ];
     const reply = await this.engine!.chat.completions.create({ messages });
