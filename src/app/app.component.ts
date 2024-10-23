ef4cca6fa188ebff0600c311ac4b260521d51160
Christian Liebel <christian.liebel@thinktecture.com>
2024-10-23 10:03:15 +0200

    Lab #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateMLCEngine, MLCEngine } from '@mlc-ai/web-llm';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   // LAB #2
+  protected readonly progress = signal(0);
+  protected readonly ready = signal(false);
+  protected engine?: MLCEngine;
+
   // LAB #3
   // LAB #5
 
   async ngOnInit() {
     // LAB #2
+    const model = 'Llama-3.2-3B-Instruct-q4f32_1-MLC';
+    this.engine = await CreateMLCEngine(model, {
+      initProgressCallback: ({ progress }) =>
+          this.progress.set(progress)
+    });
+    this.ready.set(true);
   }
 
   async runPrompt(userPrompt: string) {
